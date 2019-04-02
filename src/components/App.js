389d63882d64e2f1abd36e572de476fbdb2f4445
Permadi Wibisono <mbapewe@gmail.com>
2019-04-02 23:30:04 +0700

    Added credits

--- src/components/App.js
+++ src/components/App.js
@@ -10,9 +10,44 @@ const App = () => (
     <Container>
       <Title>Copy to Clipboard</Title>
       <SubTitle>
-        This is how to copy to clipboard using button in reactjs.
+        This is how to copy to clipboard using button in reactjs.{" "}
+        <i>No package required.</i>
       </SubTitle>
       <Example />
+    </Container>
+    <Container>
+      <SubTitle>References:</SubTitle>
+      <ul>
+        <li>
+          <a
+            href="https://reactjs.org/docs/hooks-reference.html"
+            className="button is-text"
+          >
+            React Hooks Docs
+          </a>
+        </li>
+        <li>
+          <a href="https://bulma.io/documentation" className="button is-text">
+            Bulma.io
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://hackernoon.com/copying-text-to-clipboard-with-javascript-df4d4988697f"
+            className="button is-text"
+          >
+            Copying text to clipboard with JavaScript
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.w3schools.com/howto/howto_js_snackbar.asp"
+            className="button is-text"
+          >
+            How TO - Snackbar / Toast
+          </a>
+        </li>
+      </ul>
     </Container>
   </Section>
 );
