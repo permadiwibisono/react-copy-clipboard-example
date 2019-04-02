a61144d7abdab9250b1a72a94f70de3ad844332b
Permadi Wibisono <mbapewe@gmail.com>
2019-04-02 23:19:47 +0700

    Added for read only text also can copied to clipboard

--- src/components/CopyToClipboard.js
+++ src/components/CopyToClipboard.js
@@ -1,25 +1,47 @@
-import React, { useRef } from "react";
+import React, { useRef, useState, useEffect } from "react";
+import Toast from "./Toast";
+
+const EMPTY_STR_MSG = "Nothing to copy!";
+const SUCCESS_COPIED_MSG = "Copied to clipboard!";
 
 const CopyToClipboard = props => {
-  const input = useRef(null);
+  const inputRef = useRef(null);
+  const [isShowToast, toggleToast] = useState(false);
+  const [messageToast, setMsgToast] = useState(EMPTY_STR_MSG);
+  useEffect(
+    () => {
+      const timeout = setTimeout(() => toggleToast(false), 3000);
+      return () => {
+        clearInterval(timeout);
+      };
+    },
+    [isShowToast]
+  );
   return (
     <div className="field">
       <textarea
         style={{ position: "absolute", left: "-99999px" }}
         readOnly
-        ref={input}
+        ref={inputRef}
         value={props.value}
       />
       <button
         className="button"
         onClick={() => {
-          input.current.select();
-          document.execCommand("copy");
-          alert("copied");
+          if (props.value !== "") {
+            inputRef.current.select();
+            document.execCommand("copy");
+            setMsgToast(SUCCESS_COPIED_MSG);
+            toggleToast(true);
+          } else {
+            setMsgToast(EMPTY_STR_MSG);
+            toggleToast(true);
+          }
         }}
       >
         {props.children}
       </button>
+      <Toast show={isShowToast}>{messageToast}</Toast>
     </div>
   );
 };
--- src/components/Example.js
+++ src/components/Example.js
@@ -1,17 +1,38 @@
 import React, { useState } from "react";
 import Container from "./Container";
+import SubTitle from "./Typos/SubTitle";
 import Input from "./Form/Input";
 import CopyToClipboard from "./CopyToClipboard";
 
 function Example() {
   const [value, setValue] = useState("");
+  const [website] = useState(
+    "https://github.com/permadiwibisono/react-copy-clipboard-example"
+  );
   return (
     <Container>
+      <SubTitle>With Input Text.</SubTitle>
       <div className="field">
         <Input value={value} onChange={e => setValue(e.target.value)} />
       </div>
       <div className="field">
         <CopyToClipboard value={value}>Copy to clipboard</CopyToClipboard>
+      </div>
+      <SubTitle>With Read Only Text.</SubTitle>
+      <div className="level">
+        <div className="level-item">
+          <div>
+            <p className="heading">Repo: (Dont forget to fork or star it)</p>
+            <p className="title">
+              <a className="button is-text" href={website}>
+                Click here or copy the repo link.
+              </a>
+            </p>
+          </div>
+        </div>
+        <div className="level-item">
+          <CopyToClipboard value={website}>Copy Link!</CopyToClipboard>
+        </div>
       </div>
     </Container>
   );
