07c1209708b2c578a344d3ef6e4d36eab76813f9
Permadi Wibisono <mbapewe@gmail.com>
2019-04-03 02:44:23 +0700

    Trying to fix in iphone browser bug

--- src/components/CopyToClipboard.js
+++ src/components/CopyToClipboard.js
@@ -4,6 +4,7 @@ import Toast from "./Toast";
 const EMPTY_STR_MSG = "Nothing to copy!";
 const SUCCESS_COPIED_MSG = "Copied to clipboard!";
 
+const handleCopy = () => {};
 const CopyToClipboard = props => {
   const inputRef = useRef(null);
   const [isShowToast, toggleToast] = useState(false);
@@ -20,8 +21,8 @@ const CopyToClipboard = props => {
   return (
     <div className="field">
       <textarea
-        style={{ position: "absolute", left: "-99999px" }}
-        readOnly
+        style={{ position: "absolute", left: "-9999999px" }}
+        // readOnly
         ref={inputRef}
         value={props.value}
       />
@@ -29,7 +30,19 @@ const CopyToClipboard = props => {
         className="button"
         onClick={() => {
           if (props.value !== "") {
-            inputRef.current.select();
+            if (navigator.userAgent.match(/ipad|iphone/i)) {
+              const range = document.createRange();
+              console.log(range);
+              range.selectNodeContents(inputRef.current);
+              console.log(range);
+              const selection = window.getSelection();
+              selection.removeAllRanges();
+              selection.addRange(range);
+              console.log(selection);
+              inputRef.current.setSelectionRange(0, 999999);
+            } else {
+              inputRef.current.select();
+            }
             document.execCommand("copy");
             setMsgToast(SUCCESS_COPIED_MSG);
             toggleToast(true);
